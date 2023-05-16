136c7cdf4a48dbe400d5f15d22a2f8490f15a8ce
Cyanogendark <63381944+Cyanogendark@users.noreply.github.com>
2023-05-16 12:48:47 -0400

    Update descargar.php

--- descargar.php
+++ descargar.php
@@ -1,24 +1,57 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $url = $_POST["url"];
-    $formato = $_POST["formato"];
-    
-    if ($formato == "video") {
-        // Descargar vídeo
-        $cmd = "youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' --merge-output-format mp4 " . escapeshellarg($url);
-    } else {
-        // Descargar audio
-        $cmd = "youtube-dl -x --audio-format mp3 " . escapeshellarg($url);
-    }
-    
-    exec($cmd, $output, $return);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $url = $_POST['url'];
+    $videoId = obtenerIdVideo($url);
 
-    if ($return === 0) {
-        // Descarga exitosa
-        echo "Descarga completada. Haz clic <a href='" . $output[0] . "'>aquí</a> para descargar el archivo.";
+    if ($videoId) {
+        $videoUrl = "https://www.youtube.com/watch?v={$videoId}";
+        $audioUrl = "https://www.youtube.com/watch?v={$videoId}";
+
+        // Descarga del video
+        descargarArchivo($videoUrl, 'video.mp4');
+
+        // Descarga del audio
+        descargarArchivo($audioUrl, 'audio.mp3');
     } else {
-        // Descarga fallida
-        echo "Hubo un error al descargar el archivo.";
+        echo "<p>Por favor, ingresa una URL válida de YouTube.</p>";
     }
+}
+
+function obtenerIdVideo($url) {
+    $pattern =
+        '%^# Match any YouTube URL
+        (?:https?://)?    # Optional scheme. Either http or https
+        (?:www\.)?        # Optional www subdomain
+        (?:               # Group host alternatives
+          youtu\.be/      # Either youtu.be,
+        | youtube\.com    # or youtube.com
+          (?:             # Group path alternatives
+            /embed/       # Either /embed/
+          | /v/           # or /v/
+          | /watch\?v=    # or /watch\?v=
+          )               # End path alternatives.
+        )                 # End host alternatives.
+        ([\w-]{11})       # The video ID.
+        $%x';
+
+    $result = preg_match($pattern, $url, $matches);
+
+    if ($result) {
+        return $matches[1];
+    }
+
+    return false;
+}
+
+function descargarArchivo($url, $nombreArchivo) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    header('Content-Type: application/octet-stream');
+    header("Content-Disposition: attachment; filename=\"$nombreArchivo\"");
+    echo $data;
 }
 ?>
